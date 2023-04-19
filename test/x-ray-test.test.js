b25ca1f69e1959a58fe7a17ff0efe79bd0ab0a64
houy <67390187+houstonpearse@users.noreply.github.com>
2023-04-19 11:10:21 +1000

    Initial commit

---
+++ test/x-ray-test.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const XRayTest = require('../lib/x-ray-test-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/x-ray-test-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new XRayTest.XRayTestStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
